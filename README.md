315ad2c18a76e093a61c4a72f69ba69375a91f96
Angelo Rendina <angelo.rendina@pendo.io>
2020-12-09 18:27:23 +0000

    Created

---
+++ README.md
@@ -0,0 +1,3 @@
+Run the local server with `python serve.py [port-number]` (default 8080).
+
+Navigate to `localhost:[port-number]`
---
+++ serve.py
@@ -0,0 +1,30 @@
+import http.server
+import socketserver
+import sys
+import signal
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = "/"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+handler_object = MyHttpRequestHandler
+
+try:
+    PORT = int(sys.argv[1])
+except:
+    PORT = 8080
+
+class MyServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+my_server = MyServer(("", PORT), handler_object)
+
+print("[serve.py] Started server on localhost:" + str(PORT))
+
+try:
+    my_server.serve_forever()
+except KeyboardInterrupt:
+    print("[serve.py] Stopping server on locahost:" + str(PORT))
+finally:
+    my_server.server_close()
